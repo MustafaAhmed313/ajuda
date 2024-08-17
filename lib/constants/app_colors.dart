e07180ab85d332e1e89f7e803a332c9f9dc71b64
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-08-17 14:34:56 +0300

    adding auth screen:
    - auth app bar.
    - adding new colors.
    - finishing onBoarding Screen.

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color black = Colors.black;
   static const Color primary300 = Color(0xFFFEBC12);
   static const Color white = Colors.white;
+  static const Color gray500 = Color(0xFF666D80);
 }
--- lib/screens/onboarding-screen.dart
+++ lib/screens/onboarding-screen.dart
@@ -1,6 +1,8 @@
 import 'package:donation_app/constants/app_colors.dart';
 import 'package:donation_app/constants/app_font_sizes.dart';
 import 'package:donation_app/constants/app_font_weights.dart';
+import 'package:donation_app/screens/login_screen.dart';
+import 'package:donation_app/widgets/auth_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,73 +17,77 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          width: double.infinity,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Image(image: AssetImage('assets/images/Logo.png')),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                child: const Column(
-                  children: [
-                    Image(image: AssetImage('assets/images/onboarding.png'), fit: BoxFit.cover),
-                    SizedBox(height: 50),
-                    SizedBox(
-                      width: 327,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: authAppBar(),
+      body: Container(
+            width: double.infinity,
+            color: AppColors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // const Image(image: AssetImage('assets/images/Logo.png')),
+                Padding(
+                    padding: const EdgeInsets.only(top: 90),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: const Column(
                         children: [
-                          Text('Donation made Easy',
-                            style: TextStyle(
-                              color: AppColors.black,
-                              fontSize: AppFontSizes.num18,
-                              fontWeight: AppFontWeights.semiBold,
+                          Image(image: AssetImage('assets/images/onboarding.png'), fit: BoxFit.cover),
+                          SizedBox(height: 50),
+                          SizedBox(
+                              width: 327,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Donation made Easy',
+                                    style: TextStyle(
+                                      color: AppColors.black,
+                                      fontSize: AppFontSizes.num18,
+                                      fontWeight: AppFontWeights.semiBold,
 
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text('Ajuda is a platform for philanthropists to make donations to various social programmes and charities around the world.',
-                            style: TextStyle(
-                              color: AppColors.black,
-                              fontSize: AppFontSizes.num16,
-                              fontWeight: AppFontWeights.regular
-                            )
-                          ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text('Ajuda is a platform for philanthropists to make donations to various social programmes and charities around the world.',
+                                      style: TextStyle(
+                                          color: AppColors.black,
+                                          fontSize: AppFontSizes.num16,
+                                          fontWeight: AppFontWeights.regular
+                                      )
+                                  ),
+                                ],
+                              )
+                          )
                         ],
-                      )
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: AppColors.primary300,
-                        borderRadius: BorderRadius.circular(18)
+                      ),
                     ),
-                    width: 144,
-                    height: 40,
-                    child: const Center(
-                        child: Text('Start donating' ,
-                          style: TextStyle(
-                              color:  AppColors.white,
-                              fontSize: AppFontSizes.num14,
-                              fontWeight: AppFontWeights.medium
-                          ),
-                        )
-                    )
                 ),
-              )
-            ],
-          )
-        )
+                GestureDetector(
+                  onTap: () {
+                    Get.offAll(const LoginScreen());
+                  },
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: AppColors.primary300,
+                          borderRadius: BorderRadius.circular(18)
+                      ),
+                      width: 144,
+                      height: 40,
+                      child: const Center(
+                          child: Text('Start donating' ,
+                            style: TextStyle(
+                                color:  AppColors.white,
+                                fontSize: AppFontSizes.num14,
+                                fontWeight: AppFontWeights.medium
+                            ),
+                          )
+                      )
+                  ),
+                )
+              ],
+            )
+        ),
     );
   }
 }
